2b47d2fa8878a580124d9d150efd47dcc8953d69
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-04-02 12:53:23 -0300

    Added UX Added Replay Added PainKiller

--- Jogo-Adivinhacao-ConsoleApp/Program.cs
+++ Jogo-Adivinhacao-ConsoleApp/Program.cs
@@ -1,63 +1,87 @@
-﻿namespace Jogo_De_Adivinhacao_ConsoleApp
+﻿using System.Threading.Channels;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Jogo_De_Adivinhacao_ConsoleApp
 {
     internal class Program
     {
-        static int chances;
-        static int numeroChutado, numero;
-        static int pontuacao = 1000;
-        static int[] numeros = new int[30];
 
         static void Main(string[] args)
         {
-            numero = 0;
             Console.WriteLine("Jogo de adivinhação | Academia de Programação 2024!\n");
-
-            seletorDeDificuldade();
-            int adivinhar = geradorDeNumeroAleatorio(numeros);
-            jogoDeAdivinhar(adivinhar, numeros);
+            iniciaJogo();
         }
 
-        private static void jogoDeAdivinhar(int adivinhar, int[] numeros)
+        private static void iniciaJogo()
+        {
+            int chances = 0;
+            int numeroChutado = ' ';
+            int numero = 0;
+            int pontuacao = 1000;
+            int[] numeros = new int[30];
+
+            seletorDeDificuldade(ref chances);
+            int adivinhar = geradorDeNumeroAleatorio(ref numeros, ref numero);
+            escolhaDoNumero(adivinhar, numeros, ref numeroChutado, ref pontuacao, ref chances);
+        }
+
+        static void escolhaDoNumero(int adivinhar, int[] numeros, ref int numeroChutado, ref int pontuacao, ref int chances)
         {
 
 
             do
             {
-                placar(adivinhar);
+                placar(adivinhar, ref pontuacao, ref chances);
                 numeroChutado = valor<int>($"Digite um número entre < 0 à {numeros.Length} >: ");
 
                 if (numeroChutado == adivinhar)
                 {
-                    Console.WriteLine("Acertou!\nTU É BRABO MENÓ!");
+                    Console.WriteLine("\t\t\t\tACERTOU!\n\t\tTU É BRABO MENÓ!");
+                    replayGame();
+
                 }
                 else
                 {
                     Console.WriteLine("Você errou!");
-                    respostaErrada(adivinhar);
+                    respostaErrada(adivinhar, ref numeroChutado, ref pontuacao, ref chances);
+
                 }
 
             } while (chances != 0);
         }
 
-        private static void placar(int adivinhar)
+        private static void replayGame()
+        {
+            if (continua("Deseja jogar novamente? ( S / N )") == true)
+            {
+                iniciaJogo();
+            }
+            else
+            {
+                Console.WriteLine("Obrigado por jogar :)");
+                Environment.Exit(0);
+            }
+        }
+
+        static void placar(int adivinhar, ref int pontuacao, ref int chances)
         {
             Console.Write($"\n\n\t\tSua pontuação: {pontuacao}Pts.");
             Console.Write($"\t\tVocê tem: {chances} chutes.");
             Console.WriteLine($"\t\tO Numero correto é: {adivinhar}\n\n");
         }
 
-        private static void respostaErrada(int adivinhar)
+        static void respostaErrada(int adivinhar, ref int numeroChutado, ref int pontuacao, ref int chances)
         {
             int calculoErro = Math.Abs((numeroChutado - adivinhar) / 2) * 10;
             pontuacao -= calculoErro;
             chances--;
             Console.Write($"\tVocê perdeu {calculoErro} pontos.");
-            Console.WriteLine($"\t\tVocê ainda tem:{chances}.");
+            Console.WriteLine($"\t\tVocê gastou 1 chance.");
             valor<string>("ENTER para continuar!");
             Console.Clear();
         }
 
-        private static int geradorDeNumeroAleatorio(int[] numeros)
+        static int geradorDeNumeroAleatorio(ref int[] numeros, ref int numero)
         {
 
             for (int i = 0; i < numeros.Length; i++)
@@ -69,9 +93,9 @@
             return adivinhar;
         }
 
-        private static void seletorDeDificuldade()
+        static void seletorDeDificuldade(ref int chances)
         {
-            int dificuldade = valor<int>("Escolha a dificuldade:\n1 - Fácil.\n2 - Médio.\n3 - Difícil.");
+            int dificuldade = valor<int>("Escolha a dificuldade:\n\n\t\t1 - Fácil.\t2 - Médio.\t3 - Difícil.");
 
             switch (dificuldade)
             {
